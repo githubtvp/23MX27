e057fdb4a43045cd9954a1b571c26c025c907d25
githubtvp <tvp1640@gmail.com>
2024-02-04 22:53:58 +0530

    Saving latest Updates with script

--- AndroidStudioProjects/Ps1/app/src/main/java/com/example/ps1/MainActivity.kt
+++ AndroidStudioProjects/Ps1/app/src/main/java/com/example/ps1/MainActivity.kt
@@ -6,22 +6,22 @@ import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.ViewSwitcher.ViewFactory
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val x = findViewById<TextView>(R.id.t1)
-        val z = findViewById<Button>(R.id.b1)
-        val y = findViewById<Button>(R.id.b2)
-        val w = findViewById<ImageView>(R.id.iv1)
-        z.setOnClickListener( View.OnClickListener { x.text = "PSG Tech" } )
-        y.setOnClickListener{ w.setImageResource(R.drawable.smiley2)  }
+        val x = findViewById<TextView>(R.id.lbl1)
+        val z = findViewById<Button>(R.id.btn1)
+        val w = findViewById<ImageView>(R.id.img1)
+        var n = 1;
+        z.setOnClickListener( View.OnClickListener { x.text = "   I am so full  "
+            z.text = "Done"
+            w.setImageResource(R.drawable.smiley2) } )
     }
 
-    fun btn1Clk()
+    fun btn1Clk(n : Int)
     {
-
+        if
     }
 }
