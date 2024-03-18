14cbf6e99429f805f63e101c79d03d95de18229e
githubtvp <tvp1640@gmail.com>
2024-03-18 19:21:28 +0530

    Saving latest Updates with script

---
+++ AndroidStudioProjects/MenuAndCall/app/src/main/java/com/example/menuandcall/MainActivity.kt
@@ -0,0 +1,100 @@
+package com.example.menuandcall
+
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+class MainActivity : AppCompatActivity() {
+    val c = Intent(Intent.ACTION_CALL)
+    private lateinit var t: TextView
+    private var telNo = "tel:+91-9498413977"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+      //  c.data = Uri.parse("tel:+91-9498413977" )
+        c.data = Uri.parse(telNo)
+        t= findViewById(R.id.t)
+        registerForContextMenu(t)//registers t for context menu
+    }
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.main_menu,menu)
+        menu?.setHeaderTitle("Select")
+    }
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val i:Int = item.itemId
+        when(i)
+        {
+            R.id.item1 ->
+            {
+                start()
+            }
+            R.id.item2 ->
+            {
+            }
+            R.id.item3 ->
+            {
+                whatsapp()
+            }
+        }
+        return super.onContextItemSelected(item)
+    }
+    private fun whatsapp() {
+        val installed: Boolean = appInstalledOrNot("com.whatsapp")
+        if(installed)
+        {
+            val intent = Intent(Intent.ACTION_VIEW)
+           // intent.setData(Uri.parse("https://api.whatsapp.com/send?phone="+"+919362688752" + "&text=Hai!"));
+            intent.setData(Uri.parse("https://api.whatsapp.com/send?phone="+ telNo + "&text=Hi! from Tvp"));
+                    startActivity(intent)
+        }
+        else
+        {
+            Toast.makeText(applicationContext,"Not installed",Toast.LENGTH_LONG).show()
+        }
+    }
+    private fun appInstalledOrNot(url: String): Boolean {
+        val packageManager = packageManager
+        val app_installed: Boolean
+        app_installed = try {
+            packageManager.getPackageInfo(url,
+                PackageManager.GET_ACTIVITIES)
+            true
+        } catch (e: PackageManager.NameNotFoundException) {
+            false
+        }
+        return app_installed
+    }
+    private fun start() {
+        if (ContextCompat.checkSelfPermission(this@MainActivity,
+                Manifest.permission.CALL_PHONE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this,
+                Array(1){Manifest.permission.CALL_PHONE},111)
+        } else {
+            startActivity(c)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions,
+            grantResults)
+        if(requestCode==111 &&
+            grantResults[0]==PackageManager.PERMISSION_GRANTED)
+            startActivity(c)
+    }
+}
---
+++ AndroidStudioProjects/SMSApp/app/src/main/java/com/example/smsapp/MainActivity.kt
@@ -0,0 +1,66 @@
+package com.example.smsapp
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.provider.Telephony
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main))
+        { v, insets ->
+            val systemBars =
+                insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right,
+                systemBars.bottom)
+            insets
+        }
+        if (ContextCompat.checkSelfPermission(this@MainActivity,
+                Manifest.permission.RECEIVE_SMS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(this,
+                Array(1){ Manifest.permission.RECEIVE_SMS},111)
+        } else {
+            readmessage()
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions,
+            grantResults)
+        if(requestCode==111 && grantResults[0]==
+            PackageManager.PERMISSION_GRANTED)
+            readmessage()
+    }
+    private fun readmessage()
+    {
+        val t = findViewById<TextView>(R.id.t)
+        var b = object : BroadcastReceiver() {
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                for(sms in Telephony.Sms.Intents.getMessagesFromIntent(p1))
+                {
+                    t.text = sms.displayMessageBody
+                }
+            }
+        }
+        registerReceiver(b,
+            IntentFilter("android.provider.Telephony.SMS_RECEIVED"))
+    }
+}
